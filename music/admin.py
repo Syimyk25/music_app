453d07f371a38e95d74843fc062895c29ba2efaf
Syimyk25 <csqvsr25@gmail.com>
2022-07-07 20:22:45 +0600

    new app

---
+++ music/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from music.models import Category, Music
+
+# Register your models here.
+
+class MusicAdmin(admin.ModelAdmin):
+    list_display = ['title', 'duration', 'category', 'created_at']
+    list_filter = ['category']
+    search_fields = ['id']
+
+
+
+admin.site.register(Category)
+admin.site.register(Music, MusicAdmin)
